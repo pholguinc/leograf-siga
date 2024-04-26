2d5bdc9384175575432d2ec6f79fc166af934172
Pandoro365 <fabioalianza13@htomail.com>
2024-04-26 18:04:27 -0500

    Maquetacion Modulo Modulos

--- src/app/views/admin/mantenimientos/modulos/modulos.component.ts
+++ src/app/views/admin/mantenimientos/modulos/modulos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-modulos',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./modulos.component.css']
 })
 export class ModulosComponent {
+
+  index:number = 1;
+  size:number = 100;
+  isVisible:boolean = false;
+
+  constructor(
+    private modal: NzModalService
+  ){
+
+  }
+
+  onGuardarAceptar(event:any){
+    if(event){
+      this.isVisible = false;
+    }
+  }
+
+  onCerrarCancelar(event:any){
+    if(event){
+      this.isVisible = false;
+    }
+  }
+
+  showModal(){
+    this.isVisible = true;
+  }
+
+  showDesactivarConfirm(): void {
+    this.modal.confirm({
+      nzTitle: 'Inactivar módulo',
+      nzContent: '<span>¿Está seguro de que desea inactivar el módulo?</span><br><b>Al inactivar el módulo, este ya no se mostrará en el sistema.</b>',
+      nzOkText: 'Yes',
+      nzOkType: 'primary',
+      nzOkDanger: true,
+      nzOnOk: () => console.log('OK'),
+      nzCancelText: 'No',
+      nzOnCancel: () => console.log('Cancel')
+    });
+  }
+
+  showActivarConfirm(): void {
+    this.modal.confirm({
+      nzTitle: 'Activar módulo',
+      nzContent: '<span>¿Está seguro de que desea activar el módulo?</span><br><b>Al activar el módulo, este se volverá a mostrar en el sistema.</b>',
+      nzOkText: 'Yes',
+      nzOkType: 'primary',
+      nzOnOk: () => console.log('OK'),
+      nzCancelText: 'No',
+      nzOnCancel: () => console.log('Cancel')
+    });
+  }
 
 }
--- src/app/views/admin/mantenimientos/modulos/modulos.module.ts
+++ src/app/views/admin/mantenimientos/modulos/modulos.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ModulosRoutingModule } from './modulos-routing.module';
 import { ModulosComponent } from './modulos.component';
 import { NgZorroAntdModule } from 'src/app/ng-zorro.module';
+import { ModalModulosComponent } from './modal-modulos/modal-modulos.component';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 
 @NgModule({
   declarations: [
-    ModulosComponent
+    ModulosComponent,
+    ModalModulosComponent
   ],
   imports: [
     CommonModule,
     ModulosRoutingModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    NzModalModule
   ]
 })
 export class ModulosModule { }
