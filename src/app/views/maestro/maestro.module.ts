68abbd7ba12acf5a0f2e9152fc9a51a6d11acd3b
Pandoro365 <fabioalianza13@htomail.com>
2024-04-25 19:32:47 -0500

    Fix modelo home

--- src/app/views/maestro/maestro.module.ts
+++ src/app/views/maestro/maestro.module.ts
@@ -5,6 +5,8 @@ import { MaestroRoutingModule } from './maestro-routing.module';
 import { MaestroComponent } from './maestro.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 
 
 @NgModule({
@@ -16,6 +18,8 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
     MaestroRoutingModule,
     NzLayoutModule,
     NzIconModule,
+    NzAvatarModule,
+    NzPageHeaderModule,
   ]
 })
 export class MaestroModule { }
