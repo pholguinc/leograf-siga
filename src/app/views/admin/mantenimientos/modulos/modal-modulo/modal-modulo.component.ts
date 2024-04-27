ae60c41a40e15c3a05e5fd0152962673e9488af4
Pandoro365 <fabioalianza13@htomail.com>
2024-04-26 19:37:08 -0500

    Fix avance roles

--- src/app/views/admin/mantenimientos/modulos/modal-modulo/modal-modulo.component.ts
+++ src/app/views/admin/mantenimientos/modulos/modal-modulo/modal-modulo.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-modal-modulo',
   templateUrl: './modal-modulo.component.html',
   styleUrls: ['./modal-modulo.component.css']
 })
-export class ModalModuloComponent {
+export class ModalModuloComponent implements OnInit {
+
+  @Output() onGuardarAceptar: EventEmitter<any>;
+  @Output() onCerrarCancelar: EventEmitter<any>;
+  public isVisible: boolean = true;
+  public titleModal: string = 'Agregar modulo';
+  public subDescargas: string = '(Imagen: Tipo .jpeg o .jpg. Peso 1 MB)';
+
+  constructor(
+    private modal: NzModalService
+  ){
+    this.onGuardarAceptar = new EventEmitter<any>();
+    this.onCerrarCancelar = new EventEmitter<any>();
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  handleCancel(){
+    this.isVisible = false;
+    this.onCerrarCancelar.emit(true);
+  }
+
+  showConfirm(): void {
+    this.modal.confirm({
+      nzTitle: 'Activar módulo',
+      nzContent: '<span>¿Está seguro de que desea activar el módulo?</span><br><b>Al activar el módulo, este se volverá a mostrar en el sistema.</b>',
+      nzOkText: 'Yes',
+      nzOkType: 'primary',
+      nzOnOk: () => this.handleCancel(),
+      nzCancelText: 'No',
+      nzOnCancel: () => console.log('Cancel')
+    });
+  }
 
 }
